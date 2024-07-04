0239c98f5cc4c96bcabbee178a5d21367182ece1
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-07-04 19:30:03 +0600

    Leetcode: 94. Binary Tree Inorder Traversal

--- src/RegularPractice/LeetCode94.java
+++ src/RegularPractice/LeetCode94.java
@@ -20,11 +20,11 @@ Follow up: Recursive solution is trivial, could you do it iteratively?
 
 import java.util.ArrayList;
 import java.util.List;
-class TreeNode{
+class TreeNode94{
     int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int val){
+    TreeNode94 left;
+    TreeNode94 right;
+    TreeNode94(int val){
         this.val = val;
         this.left = null;
         this.right = null;
@@ -32,11 +32,11 @@ class TreeNode{
 }
 class InorderTraversalTree{
     List<Integer> result = new ArrayList<>();
-    public List<Integer> inorderTraversal(TreeNode root){
+    public List<Integer> inorderTraversal(TreeNode94 root){
         inOrder(root);
         return result;
     }
-    public void inOrder(TreeNode root){
+    public void inOrder(TreeNode94 root){
         if (root == null){
             return;
         }
@@ -48,21 +48,21 @@ class InorderTraversalTree{
 public class LeetCode94 {
     public static void main(String[] args) {
         // Example 1
-        TreeNode root1 = new TreeNode(1);
-        root1.right = new TreeNode(2);
-        root1.right.left = new TreeNode(3);
+        TreeNode94 root1 = new TreeNode94(1);
+        root1.right = new TreeNode94(2);
+        root1.right.left = new TreeNode94(3);
         InorderTraversalTree traversal1 = new InorderTraversalTree();
         List<Integer> result1 = traversal1.inorderTraversal(root1);
         System.out.println(result1); // Output: [1, 3, 2]
 
         // Example 2
-        TreeNode root2 = null;
+        TreeNode94 root2 = null;
         InorderTraversalTree traversal2 = new InorderTraversalTree();
         List<Integer> result2 = traversal2.inorderTraversal(root2);
         System.out.println(result2); // Output: []
 
         // Example 3
-        TreeNode root3 = new TreeNode(1);
+        TreeNode94 root3 = new TreeNode94(1);
         InorderTraversalTree traversal3 = new InorderTraversalTree();
         List<Integer> result3 = traversal3.inorderTraversal(root3);
         System.out.println(result3); // Output: [1]
