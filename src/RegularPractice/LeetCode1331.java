7a0856e0fec99f7dca6006bfcb0d3dc76da6b04e
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-10-03 18:47:27 +0600

    Leetcode: 1331. Rank Transform of an Array

--- src/RegularPractice/LeetCode1331.java
+++ src/RegularPractice/LeetCode1331.java
@@ -4,7 +4,6 @@ package RegularPractice;
 The rank represents how large the element is. The rank has the following rules:
 Rank is an integer starting from 1.
 The larger the element, the larger the rank. If two elements are equal, their rank must be the same.Rank should be as small as possible.
-
 Example 1:
 Input: arr = [40,10,20,30]
 Output: [4,1,2,3]
@@ -21,8 +20,7 @@ Constraints:
 -109 <= arr[i] <= 10
 * */
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 class RankTransformArray{
     public int [] arrayRank( int [] arr){
