a02ecc440552529989f8ee518a2057506c53c97d
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-12-09 18:29:01 +0600

    Leetcode: 1760. Minimum Limit of Balls in a Bag

--- src/RegularPractice/LeetCode1760.java
+++ src/RegularPractice/LeetCode1760.java
@@ -59,5 +59,6 @@ public class LeetCode1760 {
         System.out.println(obj.minimumSize(new int[]{9}, 2));
         System.out.println(obj.minimumSize(new int[]{2,4,8,2}, 4));
         System.out.println(obj.minimumSize(new int[]{6,9,12,24}, 5));
+        System.out.println(obj.minimumSize(new int[]{1,2,3,4,5,6,7,8,9}, 6));
     }
 }
