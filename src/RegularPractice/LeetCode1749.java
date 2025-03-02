302d251f63be708fcff307a2161106313c544f48
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2025-03-02 23:45:46 +0600

    Leetcode: 1749. Maximum Absolute Sum of Any Subarray

--- src/RegularPractice/LeetCode1749.java
+++ src/RegularPractice/LeetCode1749.java
@@ -5,17 +5,14 @@ package RegularPractice;
 Return the maximum absolute sum of any (possibly empty) subarray of nums.
 
 Note that abs(x) is defined as follows:
-
 If x is a negative integer, then abs(x) = -x.
 If x is a non-negative integer, then abs(x) = x.
 
 Example 1:
-
 Input: nums = [1,-3,2,3,-4]
 Output: 5
 Explanation: The subarray [2,3] has absolute sum = abs(2+3) = abs(5) = 5.
 Example 2:
-
 Input: nums = [2,-5,1,-4,3,-2]
 Output: 8
 Explanation: The subarray [-5,1,-4] has absolute sum = abs(-5+1-4) = abs(-8) = 8.
